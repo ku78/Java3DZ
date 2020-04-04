022c5c0fc87a2fcde1d3ab18d9ea2ed5d8e84310
Vitalya Zvyagin <78ku78@gmail.com>
2020-04-04 20:03:46 +0300

    dz1

--- src/main/java/lesson1/z1/Arrays.java
+++ src/main/java/lesson1/z1/Arrays.java
@@ -1,7 +1,34 @@
 package lesson1.z1;
-
+import java.util.Scanner;
 //Написать метод, который меняет два элемента массива местами (массив может быть любого ссылочного типа);
 
-public class Arrays {
+public class Arrays<T> {
+    private T[] arr;
 
+
+    public Arrays(T...arr) {
+        this.arr = arr;
+    }
+
+    public void edit() {
+        Object arrN[] = new Object[arr.length];
+        Scanner scan1 = new Scanner(System.in);
+        Scanner scan2 = new Scanner(System.in);
+
+
+        System.out.println("Введите индекс от 0 до "+(arr.length-1));
+        int index1 = scan1.nextInt();
+        int index2 = scan2.nextInt();
+        for (int i = 0; i <arr.length ; i++) {
+            if(i==index1){
+                arrN[i]=arr[index2];
+            }else if(i==index2){
+                arrN[i]=arr[index1];
+            }else{arrN[i]=arr[i];}
+            System.out.println("Блыло: "+arr[i]+" Стало: "+arrN[i]);
+        }
+
+        System.out.println("\ud83d\ude3e "+"\ud83d\ude3e "+"\ud83d\ude3e "+"\ud83d\ude3e "+"\ud83d\ude3e "+
+                "\ud83d\ude3e "+"\ud83d\ude3e "+"\ud83d\ude3e "+"\ud83d\ude3e "+"\ud83d\ude3e "+"\ud83d\ude3e ");
+    }
 }
--- src/main/java/lesson1/z1/EditItems.java
+++ src/main/java/lesson1/z1/EditItems.java
@@ -4,6 +4,10 @@ package lesson1.z1;
 
 public class EditItems {
     public static void main(String[] args) {
+        Arrays<String> arr = new Arrays<>("V","i","t","a","l","i","k");
+        Arrays<Integer> arr2 = new Arrays<>(5,3,4,5,6,7,8,3);
+        arr.edit();
+        arr2.edit();
 
     }
 }
