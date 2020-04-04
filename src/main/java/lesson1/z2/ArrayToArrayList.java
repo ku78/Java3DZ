9b297af6af859b25fae79029b771a5cc91c98513
Vitalya Zvyagin <78ku78@gmail.com>
2020-04-04 21:37:39 +0300

    dz1_z2

--- src/main/java/lesson1/z2/ArrayToArrayList.java
+++ src/main/java/lesson1/z2/ArrayToArrayList.java
@@ -3,7 +3,10 @@ package lesson1.z2;
 //2. Написать метод, который преобразует массив в ArrayList;
 public class ArrayToArrayList {
     public static void main(String[] args) {
-
+        Arrays<String> arr1 = new Arrays<>("V", "i", "t", "a", "l", "i", "k");
+        Arrays<Integer> arr2 = new Arrays<>(5, 3, 4, 5, 6, 7, 8, 3);
+        arr1.changes();
+        arr2.changes();
 
 }
 }
--- src/main/java/lesson1/z2/Arrays.java
+++ src/main/java/lesson1/z2/Arrays.java
@@ -1,6 +1,21 @@
 package lesson1.z2;
+import java.util.ArrayList;
+import java.util.Collections;
+//2. Написать метод, который преобразует массив в ArrayList;
+public class Arrays<T>{
+    private T[] arr;
+    ArrayList<T> arrChan = new ArrayList<T>();
+    public Arrays(T...arr) {
+        this.arr = arr;
+    }
+    public void changes(){
+        Collections.addAll(arrChan,arr );
+        System.out.println("\nArray в ArrayList с помощью метода Collections.addAll()");
+        System.out.print(arrChan.getClass()+": ");
+        for (Object arr : arrChan)
+            System.out.print(" " + arr);
 
-public class Arrays{
+    }
 
 
 }
