a4b861687a79cc0c97f193ce84c2ac35c389918b
Vitalya Zvyagin <78ku78@gmail.com>
2020-04-04 19:59:26 +0300

    dz1

---
+++ src/main/java/lesson1/z1/EditItems.java
@@ -0,0 +1,10 @@
+package lesson1.z1;
+
+//Написать метод, который меняет два элемента массива местами (массив может быть любого ссылочного типа);
+
+public class EditItems {
+    public static void main(String[] args) {
+
+    }
+}
+
