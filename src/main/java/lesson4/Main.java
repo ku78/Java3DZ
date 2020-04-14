08100e1f19a98c7c47dc03dcb8f711fdec50600d
Vitalya Zvyagin <78ku78@gmail.com>
2020-04-15 02:20:22 +0300

    dz3

---
+++ src/main/java/lesson4/Main.java
@@ -0,0 +1,13 @@
+package lesson4;
+/**1. Создать три потока, каждый из которых выводит определенную букву (A, B и C)
+ * 5 раз (порядок – ABСABСABС). Используйте wait/notify/notifyAll.*/
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+}
