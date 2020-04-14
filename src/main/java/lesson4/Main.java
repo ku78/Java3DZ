c3c6fc06f1b92f4c19247f07f60ed81a7b31c33f
Vitalya Zvyagin <78ku78@gmail.com>
2020-04-15 02:28:31 +0300

    dz4

--- src/main/java/lesson4/Main.java
+++ src/main/java/lesson4/Main.java
@@ -3,8 +3,18 @@ package lesson4;
  * 5 раз (порядок – ABСABСABС). Используйте wait/notify/notifyAll.*/
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
+        MyThread threadA = new MyThread("A", 1);
+        MyThread threadB = new MyThread("B", 2);
+        MyThread threadC = new MyThread("C", 3);
 
+        try {
+            threadA.join();
+            threadB.join();
+            threadC.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
 
     }
--- src/main/java/lesson4/MyThread.java
+++ src/main/java/lesson4/MyThread.java
@@ -1,6 +1,39 @@
 package lesson4;
 
 public class MyThread extends Thread {
+    private int nomer;
 
+    static final Object obj = new Object();
+    static volatile int a = 1;
+    static volatile int numberOfTimes = 0;
+    public MyThread(String name,int nomer) {
+        super(name);
+        start();
+        this.nomer = nomer;
+
+    }
+
+    @Override
+    public void run() {
+        synchronized (obj){
+            while (numberOfTimes<5) {
+                if (a == nomer) {
+                    System.out.print(getName());
+                    a++;
+                    if (a>3) {
+                        a = 1;
+                        numberOfTimes++;
+                    }//end if
+                    obj.notifyAll();
+                } else { //end if
+                    try {
+                        obj.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }//end catch
+                }//end else
+            }//end while
+        }//end synchronized (obj)
+    }
 }
 
